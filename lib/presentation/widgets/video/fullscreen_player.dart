3e22733f197098d2431b59bba0362232d377973e
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-16 11:35:15 -0600

    :sparkles: feat: add GestureDetection to the video

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -44,15 +44,23 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             ));
           }
 
-          return AspectRatio(
-              aspectRatio: controller.value.aspectRatio,
-              child: Stack(children: [
-                VideoPlayer(controller),
-                Positioned(
-                    bottom: 50,
-                    left: 20,
-                    child: _VideoCaption(caption: widget.caption))
-              ]));
+          return GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: Stack(children: [
+                  VideoPlayer(controller),
+                  Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(caption: widget.caption))
+                ])),
+          );
         });
   }
 }
